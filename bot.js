c8781a6195ff8270c8f6222f2b71bc6b933a2b18
dpAbderahman <69482095+dpAbderahman@users.noreply.github.com>
2020-08-10 21:56:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['700023663218655303'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
